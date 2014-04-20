5f1b5a35c13bdf79a74c711fe8173219a999705b
simara001 <aramis001@gmail.com>
2014-04-20 10:06:20 -0700

    Deshaciendo mi pendejada

---
+++ RESideMenuStoryboardsExample/EventsTableViewCell.h
@@ -0,0 +1,20 @@
+//
+//  EventsTableViewCell.h
+//  RESideMenuStoryboardsExample
+//
+//  Created by migue ramirez on 4/20/14.
+//  Copyright (c) 2014 Roman Efimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventsTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIView *background;
+@property (weak, nonatomic) IBOutlet UIImageView *eventLogo;
+@property (weak, nonatomic) IBOutlet UILabel *eventName;
+@property (weak, nonatomic) IBOutlet UILabel *eventCapacity;
+@property (weak, nonatomic) IBOutlet UIButton *eventStatistics;
+@property (weak, nonatomic) IBOutlet UIButton *eventAttendance;
+
+@end
---
+++ RESideMenuStoryboardsExample/EventsTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  EventsTableViewCell.m
+//  RESideMenuStoryboardsExample
+//
+//  Created by migue ramirez on 4/20/14.
+//  Copyright (c) 2014 Roman Efimov. All rights reserved.
+//
+
+#import "EventsTableViewCell.h"
+
+@implementation EventsTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
