b7444a996d644cf7b56f55f33d62e731405f45d3
Fer Dingler <ferdingler@gmail.com>
2014-04-20 10:20:57 -0700

    Pie chart integrated

---
+++ RESideMenuStoryboardsExample/HTTPRequest.m
@@ -0,0 +1,76 @@
+//
+//  HTTPRequest.m
+//  Employee Photo Uploader
+//
+//  Created by Fernando Dingler on 11/11/13.
+//  Copyright (c) 2013 Deo Consultores. All rights reserved.
+//
+
+#import "HTTPRequest.h"
+
+@implementation HTTPRequest
+
+NSString *serverURL = @"http://54.86.66.130:8080/";
+NSMutableData *webData;
+
+
+-(void) sendMessage:(NSString *)msg ToURL:(NSString *)url
+{
+    webData = [NSMutableData data];
+    
+    NSLog(@"Sending: %@", msg);
+    
+    NSString *serverurl = [serverURL stringByAppendingString:url];
+    
+    NSLog(@"dest: %@", serverurl);
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:serverurl]];
+    
+    NSString *params = [[NSString alloc] initWithFormat:@"data=%@", msg];
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:[params dataUsingEncoding:NSUTF8StringEncoding]];
+    NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    
+    if(!theConnection){
+        NSLog(@"Connection error.");
+    }
+}
+
+-(void) doGet:(NSString *)url
+{
+    webData = [NSMutableData data];
+    
+    NSString *serverurl = [serverURL stringByAppendingString:url];
+    
+    NSLog(@"dest: %@", serverurl);
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:serverurl]];
+    
+    [request setHTTPMethod:@"GET"];
+    NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    
+    if(!theConnection){
+        NSLog(@"Connection error.");
+    }
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    [webData setLength: 0];
+    
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    [webData appendData:data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSError *error;
+    NSDictionary *response = [NSJSONSerialization JSONObjectWithData:webData options:kNilOptions error:&error];
+    
+    [_delegate didReceiveResponse:response];
+}
+
+@end
--- RESideMenuStoryboardsExample/PlannerStatisticsViewController.m
+++ RESideMenuStoryboardsExample/PlannerStatisticsViewController.m
@@ -26,7 +26,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view.
+    _maxAttendees = 350;
+    
+    self.slices = [NSMutableArray arrayWithCapacity:2];
+    [_slices addObject:[NSNumber numberWithInt:0]];
+    [_slices addObject:[NSNumber numberWithInt:100]];
+    
+    [self.pieChartLeft setDataSource:self];
+    [self.pieChartLeft setStartPieAngle:M_PI_2];
+    [self.pieChartLeft setAnimationSpeed:1.0];
+    [self.pieChartLeft setLabelFont:[UIFont fontWithName:@"DBLCDTempBlack" size:24]];
+    [self.pieChartLeft setLabelRadius:0];
+    [self.pieChartLeft setShowPercentage:YES];
+    [self.pieChartLeft setPieBackgroundColor:[UIColor colorWithWhite:0.95 alpha:1]];
+    [self.pieChartLeft setUserInteractionEnabled:NO];
+    [self.pieChartLeft setLabelShadowColor:[UIColor blackColor]];
+    
+    self.sliceColors =[NSArray arrayWithObjects:
+                       [UIColor colorWithRed:246/255.0 green:155/255.0 blue:0/255.0 alpha:1],
+                       [UIColor colorWithRed:129/255.0 green:195/255.0 blue:29/255.0 alpha:1],
+                       [UIColor colorWithRed:62/255.0 green:173/255.0 blue:219/255.0 alpha:1],
+                       [UIColor colorWithRed:229/255.0 green:66/255.0 blue:115/255.0 alpha:1],
+                       [UIColor colorWithRed:148/255.0 green:141/255.0 blue:139/255.0 alpha:1],nil];
+    
+    // Init HTTP
+    _request = [HTTPRequest new];
+    _request.delegate = self;
+    
+    [_request doGet:@"checkin/attendees"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,15 +64,47 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+//HTTP Request Protocol Methods
+-(void) didReceiveResponse:(NSDictionary *)serverResponse
 {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    int attendees = [[serverResponse objectForKey:@"attendees"] intValue];
+    [self refreshPieChart:attendees];
 }
-*/
+
+-(void) refreshPieChart:(int) attendees{
+    int ocupancy = (attendees * 100) / _maxAttendees;
+    int vacancy = 100 - ocupancy;
+    _ocupancyLabel.text = [NSString stringWithFormat:@"%d%%", ocupancy];
+    [self.slices setObject:[NSNumber numberWithInt:ocupancy] atIndexedSubscript:0];
+    [self.slices setObject:[NSNumber numberWithInt:vacancy] atIndexedSubscript:1];
+    [self.pieChartLeft reloadData];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self.pieChartLeft reloadData];
+}
+
+#pragma mark - XYPieChart Data Source
+
+- (NSUInteger)numberOfSlicesInPieChart:(XYPieChart *)pieChart
+{
+    return self.slices.count;
+}
+
+- (CGFloat)pieChart:(XYPieChart *)pieChart valueForSliceAtIndex:(NSUInteger)index
+{
+    return [[self.slices objectAtIndex:index] intValue];
+}
+
+- (UIColor *)pieChart:(XYPieChart *)pieChart colorForSliceAtIndex:(NSUInteger)index
+{
+    return [self.sliceColors objectAtIndex:(index % self.sliceColors.count)];
+}
+
+- (IBAction)refreshBtnPressed:(id)sender {
+    [_request doGet:@"checkin/attendees"];
+}
 
 @end
