fe09cd2f0f80ea9318637be5480c2499b80657e1
simara001 <aramis001@gmail.com>
2014-04-20 10:14:15 -0700

    Adding colors palette

---
+++ RESideMenu/UIColor+coolColors.h
@@ -0,0 +1,21 @@
+//
+//  UIColor+coolColors.h
+//  RESideMenuStoryboardsExample
+//
+//  Created by migue ramirez on 4/20/14.
+//  Copyright (c) 2014 Roman Efimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (coolColors)
+
++ (UIColor *) coolBlue;
++ (UIColor *) coolGreen;
++ (UIColor *) coolYellow;
++ (UIColor *) coolRed;
++ (UIColor *) coolGray;
++ (UIColor *) coolDarkBlue;
++ (UIColor *) coolPurple;
+
+@end
---
+++ RESideMenu/UIColor+coolColors.m
@@ -0,0 +1,35 @@
+//
+//  UIColor+coolColors.m
+//  RESideMenuStoryboardsExample
+//
+//  Created by migue ramirez on 4/20/14.
+//  Copyright (c) 2014 Roman Efimov. All rights reserved.
+//
+
+#import "UIColor+coolColors.h"
+
+@implementation UIColor (coolColors)
+
++ (UIColor *) coolBlue {
+    return [UIColor colorWithRed:0.212 green:0.725 blue:0.941 alpha:1.0];
+}
++ (UIColor *) coolGreen {
+    return [UIColor colorWithRed:0.239 green:0.816 blue:0.729 alpha:1.0];
+}
++ (UIColor *) coolYellow {
+    return [UIColor colorWithRed:0.984 green:0.694 blue:0.137 alpha:1.0];
+}
++ (UIColor *) coolRed {
+    return [UIColor colorWithRed:0.973 green:0.286 blue:0.302 alpha:1.0];
+}
++ (UIColor *) coolGray {
+    return [UIColor colorWithRed:0.906 green:0.906 blue:0.906 alpha:1.0];
+}
++ (UIColor *) coolDarkBlue {
+    return [UIColor colorWithRed:0.212 green:0.275 blue:0.325 alpha:1.0];
+}
++ (UIColor *) coolPurple {
+    return [UIColor colorWithRed:140./255 green:66./255. blue:226./255. alpha:1.0];
+}
+
+@end
